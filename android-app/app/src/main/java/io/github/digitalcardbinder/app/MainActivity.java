aa0b9e41f449d121d8c7c86e31244249e35e8285
Youngdeukseo <128191608+Youngdeukseo@users.noreply.github.com>
2026-08-27 20:00:41 +0900

    Complete fullscreen TWA test app setup

--- android-app/app/src/main/java/io/github/digitalcardbinder/app/MainActivity.java
+++ android-app/app/src/main/java/io/github/digitalcardbinder/app/MainActivity.java
@@ -1,32 +1,6 @@
 package io.github.digitalcardbinder.app;
 
-import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
+import com.google.androidbrowserhelper.trusted.LauncherActivity;
 
-import androidx.browser.customtabs.CustomTabColorSchemeParams;
-import androidx.browser.customtabs.CustomTabsIntent;
-
-public class MainActivity extends Activity {
-    private static final String START_URL = "https://digital-card-binder.github.io/";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        CustomTabColorSchemeParams colorParams = new CustomTabColorSchemeParams.Builder()
-                .setToolbarColor(0xFFFFFFFF)
-                .setNavigationBarColor(0xFFFFFFFF)
-                .build();
-
-        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder()
-                .setDefaultColorSchemeParams(colorParams)
-                .setShowTitle(false)
-                .setUrlBarHidingEnabled(true)
-                .setShareState(CustomTabsIntent.SHARE_STATE_OFF)
-                .build();
-
-        customTabsIntent.launchUrl(this, Uri.parse(START_URL));
-        finish();
-    }
+public class MainActivity extends LauncherActivity {
 }
