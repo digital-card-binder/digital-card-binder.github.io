4483fd1dc6bedae433ec3754ea5fac57ea5511d6
Youngdeukseo <128191608+Youngdeukseo@users.noreply.github.com>
2026-09-18 22:42:03 +0900

    Update series catalog total for ADV

--- collector-collection-registry.js
+++ collector-collection-registry.js
@@ -50,7 +50,7 @@
       href: "./series.html",
       documentId: "seriesDex",
       unit: "장",
-      catalogCount: 10321,
+      catalogCount: 10433,
       defaultDashboardVisible: true,
     },
     pokemon: {
